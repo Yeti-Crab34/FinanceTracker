86235648b96419b6ac24040f836472b701678de9
David Kagan <davidckagan@sbcglobal.net>
2022-07-16 11:19:53 -0400

    finished authentication

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -8,32 +8,32 @@ const userController = {};
 * createUser - create and save a new User into the database.
 */
 userController.createUser = async (req, res, next) => {
-  console.log('just entered createUser');
   try {
-    // console.log('The body is:', req.body);
     const { fullname, password, email } = req.body;
 
-    /* 
-      How do we error handle two different async functions with one catch block?
-    */
     //hash pw
     const hashedPW = await bcrypt.hash(password, 10);
-    // console.log(hashedPW);
+
+    /* for sqlQuery involving insert need to insert through $params not string literals */
+    const params = [ fullname, hashedPW, email ];
     const sqlQuery = `
       INSERT INTO users (fullname, password, email) 
-      VALUES (${fullname}, ${password}, ${email});`;
+      VALUES ($1, $2, $3);`;
     
-    console.log('querying the database');
-    const createdUser = await User.query(sqlQuery);
-    console.log('finished querying the database'); 
-    console.log(createdUser);
-    
-    res.locals.newUser = createdUser;
-    console.log('were going back to the router');
+    const createdUser = await User.query(sqlQuery, params);
     next();
   }
+   /* 
+      How do we error handle two different async functions with one catch block?
+    */
   catch (err) {
-    next ('global error handler')
+    next ({
+      log: 'Error at middleware userController',
+      status: 501,
+      message: {
+          err: `Error has occured while signing up.`,
+      },
+  })
   }
 };
 
@@ -42,9 +42,41 @@ userController.createUser = async (req, res, next) => {
 * the appropriate user in the database, and then authenticate the submitted password
 * against the password stored in the database.
 */
-userController.verifyUser = (req, res, next) => {
+userController.verifyUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    
+    //checking if both fields are filled in
 
+    if (email === undefined || password === undefined) {
+      // redirect to sign up page
+    }
+
+    const sqlQuery = `SELECT * FROM users WHERE email='${email}';`
+
+    const verifiedUser = await User.query(sqlQuery);
+    if (verifiedUser.rows.length === 0) {
+      console.log('Wrong email/password'); 
+      res.redirect(400, '/');
+    }
+    else {
+      const verifyPW = await bcrypt.compare(password, verifiedUser.rows[0].password)
+      if (verifyPW) {
+        console.log('verified user');
+        next();
+      }// TO DO else redirect to sign up page
+      else {
+        console.log('Wrong email/password');
+        res.redirect(400, '/');
+      } 
+      };
+    }
+  catch (err) {
+    next ('global error handler')
+  }
 };
 
 
 module.exports = userController;
+
+
--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -5,12 +5,13 @@ const userRouter = express.Router();
 const userController = require('../controllers/userController');
 
 userRouter.post('/signup', userController.createUser, (req, res) => {
-    return res.send(res.locals.newUser).json();
+    // TO DO REDIRECT TO DASHBOARD PAGE
+    return res.status(200).send('Created new user!').json();
 });
 
-
 userRouter.post('/login', userController.verifyUser, (req, res) => {
-
+    // TO DO REDIRECT TO DASHBOARD PAGE
+    return res.send('found user!');
 });
 
 
